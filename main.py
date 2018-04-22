a9cffd2ac4d34fee2e751b0fcb9ec45bd1ea8c76
Tomasz Garbus <tgarbus@chromium.org>
2018-04-22 12:21:17 +0200

    WIP

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from math import sqrt
 from typing import Iterable, List
+import numpy as np
 
-class MnistTrainer(object):
+# TODO:
+# * achieve accuracy 99.1%
+# * add batch normalization
+# * find 10 patches that excite the network the most
+
+
+class MnistTrainer:
     def __init__(self,
                  num_conv_layers: int = 1,
-                 dense_layers: Iterable[int] = [64, 64, 10]) -> None:
+                 dense_layers: Iterable[int] = [64, 64, 10],
+                 kernel_size: [int, int] = [10, 10],
+                 filters: int = 10) -> None:
         self.num_conv_layers = num_conv_layers
         self.dense_layers = dense_layers
+        self.kernel_size = kernel_size
+        self.filters = filters
+        self.mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
     def create_model(self) -> None:
         self.x = tf.placeholder(tf.float32, [None, 784], name='x')
@@ -16,11 +28,11 @@ class MnistTrainer(object):
 
         # Reshape input for convolution layers
         signal = tf.reshape(self.x, [-1, 28, 28, 1])
-        tmpsum = tf.summary.image(name='signal', tensor=signal)
+        # Log input images
+        tf.summary.image(name='signal', tensor=signal)
 
         # Apply convolution layers
-        self.conv_layers = [None for _ in range(self.num_conv_layers)]
-        for l in range(self.num_conv_layers):
+        for layer_no in range(self.num_conv_layers):
             # Initialize weights with stdev=sqrt(2/N)
             input_size = int(signal.get_shape()[1])
             w_init = tf.initializers.random_normal(stddev=sqrt(2 / input_size))
@@ -28,37 +40,76 @@ class MnistTrainer(object):
 
             # Build convolution layer
             cur_conv_layer = tf.layers.conv2d(inputs=signal,
-                                              filters=10,
-                                              kernel_size=[10, 10],
+                                              filters=self.filters,
+                                              kernel_size=self.kernel_size,
                                               padding='same',
                                               activation=tf.nn.relu,
                                               kernel_initializer=w_init,
                                               bias_initializer=b_init)
 
-            for i in range(10):
-                if l == 0:
-                    tmp_str = 'conv2d/kernel:0'
-                else:
-                    tmp_str = 'conv2d_%d/kernel:0' % l
-                kernel = [v for v in tf.global_variables() if v.name == tmp_str][0]
-                if l == 0:
-                    tmp_str = 'conv2d/bias:0'
-                else:
-                    tmp_str = 'conv2d_%d/bias:0' % l
-                bias = [v for v in tf.global_variables() if v.name == tmp_str][0]
-                tf.summary.image('conv%d%d_kernel' % (l, i),
-                                 tf.reshape(tf.slice(kernel,
-                                          begin=[0, 0, 0, i],
-                                          size=[10, 10, 1, 1]),
-                                            [1, 10, 10, 1]
-                                            ),
-                                 max_outputs=3)
-
-            self.conv_layers[l] = cur_conv_layer
-
             # Apply average pooling, reducing image dimensions in half
             cur_pool = tf.layers.average_pooling2d(inputs=cur_conv_layer, pool_size=[2, 2], strides=2)
+
+
+            self.exciting_patches = []
+            # Find exciting patches
+            for filter_no in range(self.filters):
+                inp = 28 // (2**layer_no)
+                single_filtered_flattened = tf.reshape(cur_conv_layer[:, :, :, filter_no], [-1])
+                top10_vals, top10_indices = tf.nn.top_k(single_filtered_flattened,
+                                    k=10,
+                                    sorted=True)
+                top10_reshaped = tf.map_fn(lambda i: [i//(inp**2), (i//inp)%inp, i%inp],
+                                           top10_indices,
+                                           dtype=[tf.int32, tf.int32, tf.int32])
+                def fun(a):
+                    sample_no, y, x = a
+                    padding = [[0, 0],
+                               [0, self.kernel_size[0]],
+                               [0, self.kernel_size[1]],
+                               [0, 0]]
+                    padded = tf.pad(signal, padding)
+                    return padded[sample_no, x:x+self.kernel_size[0], y:y+self.kernel_size[1], :]
+                exciting_patches = tf.map_fn(fun,
+                                             top10_reshaped,
+                                             dtype=tf.float32)
+                self.exciting_patches.append(exciting_patches)
+                if layer_no == 0:
+                    tf.summary.image("exciting_patches%d".format(filter_no), tf.reshape(exciting_patches[0,:,:,:], [1]+self.kernel_size+[1]))
+                self.dupa = exciting_patches
+                #self.dupa = top10_reshaped
+                """max_pos_each_sample = tf.argmax(single_filtered_flattened, axis=1)
+                num_samples = self.mb_size
+                max_each_sample_applied = tf.map_fn(lambda i: single_filtered_flattened[i][max_pos_each_sample[i]],
+                                                    np.array(list(range(0, num_samples)), dtype='int32'),
+                                                    dtype='float')
+                max_each_sample_applied = tf.reshape(max_each_sample_applied, [self.mb_size])
+                # b = tf.argmax(a, axis=1)
+                # c = tf.map_fn(lambda i: a[i][b[i]], np.array([0, 1, 2, 3], dtype='int32'))
+                # d = tf.argmax(c, axis=0)
+
+                self.dupa = #max_pos_each_sample
+                max_each_sample_applied"""
+
+            # Set pooled image as new signal
             signal = cur_pool
+
+            # Write summaries
+            for i in range(self.filters):
+                if layer_no == 0:
+                    tmp_str = 'conv2d/kernel:0'
+                else:
+                    tmp_str = 'conv2d_%d/kernel:0' % layer_no
+                kernel = [v for v in tf.global_variables() if v.name == tmp_str][0]
+                kernel = kernel[:, :, :, i]
+                kernel = tf.reshape(kernel, [-1] + self.kernel_size + [1])
+                multed = tf.reshape(cur_conv_layer[:, :, :, i], [-1, 28, 28, 1])
+                tf.summary.image('conv%d%d_kernel' % (layer_no, i),
+                                 kernel,
+                                 max_outputs=3)
+                tf.summary.image('conv%d%d_multiplied' % (layer_no, i),
+                                 multed,
+                                 max_outputs=3)
 
         # Flatten for the use in dense layers
         if self.num_conv_layers > 0:
@@ -79,6 +130,7 @@ class MnistTrainer(object):
                                         kernel_initializer=w_init,
                                         bias_initializer=b_init)
             signal = cur_layer
+
         # Apply last dense layer
         cur_layer = tf.layers.dense(inputs=signal,
                                     units=self.dense_layers[-1])
@@ -96,13 +148,15 @@ class MnistTrainer(object):
         print('list of variables', list(map(lambda x: x.name, tf.global_variables())))
 
     def train_on_batch(self, batch_x, batch_y) -> List:
-        results = self.sess.run([self.loss, self.accuracy, self.merged_summary, self.train_op],
+        results = self.sess.run([self.loss, self.accuracy, self.merged_summary, self.train_op, self.dupa],
                                 feed_dict={self.x: batch_x, self.y: batch_y})
         writer = tf.summary.FileWriter(logdir='summary')
         writer.add_summary(results[2])
-        return results[:-2]
+        return results[:2]
 
     def test_on_batch(self, batch_x, batch_y) -> List:
+        # Note that this function does not fetch |self.train_op|, so that the weights
+        # are not updated.
         results = self.sess.run([self.loss, self.accuracy, self.merged_summary],
                                 feed_dict={self.x: batch_x, self.y: batch_y})
         return results[:-1]
@@ -110,10 +164,9 @@ class MnistTrainer(object):
     def train(self) -> None:
         with tf.Session() as self.sess:
             mb_size = 256
-            nb_epochs = 1500
+            nb_epochs = 8000
             self.create_model()
             tf.global_variables_initializer().run()
-            self.mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
             for i in range(nb_epochs):
                 batch_x, batch_y = self.mnist.train.next_batch(mb_size)
                 results = self.train_on_batch(batch_x, batch_y)
