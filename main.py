dcda06182f2e6bc3b116a2f36d1f520ebc36c639
Tomasz Garbus <tgarbus@chromium.org>
2018-05-03 22:15:08 +0200

    WIP

--- main.py
+++ main.py
@@ -10,19 +10,33 @@ import logging
 # * add batch normalization (DONE)
 # * find 10 patches that excite the network the most
 
+
 class MnistTrainer:
     def __init__(self,
-                 num_conv_layers: int = 2,
+                 conv_layers: Iterable[int] = [10, 10],
                  dense_layers: Iterable[int] = [64, 64, 10],
                  kernel_size: [int, int] = [5, 5],
-                 num_filters: int = 10) -> None:
-        self.num_conv_layers = num_conv_layers
+                 nb_epochs: int = 100000,
+                 learning_rate: int = 0.2,
+                 lr_decay_time: int = 10000,
+                 mb_size: int = 100,
+                 dropout_rate: float = 0.3) -> None:
+        # Number of filters in each convolutional layer.
+        self.conv_layers = conv_layers
+        # Number of neurons in each dense layer.
         self.dense_layers = dense_layers
+        # Kernel size, common for convolutional layers.
         self.kernel_size = kernel_size
-        self.num_filters = num_filters
+        self.learning_rate = learning_rate
+        self.nb_epochs = nb_epochs
+        # Each |lr_decay_time| epochs, learning rate is divided by 2.
+        self.lr_decay_time = lr_decay_time
+        # Mini batch size.
+        self.mb_size = mb_size
+        # Dropout after each dense layer (excluding last).
+        self.dropout_rate = dropout_rate
+
         self.mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-        self.mb_size = 50
-        self.learning_rate = 0.2
 
         # Initialize logging.
         self.logger = logging.Logger("main_logger", level=logging.INFO)
@@ -43,21 +57,23 @@ class MnistTrainer:
         self.y = tf.placeholder(tf.float32, [self.mb_size, 10], name='y')
 
         # Initialize fetch handles for exciting patches and their respective responses.
-        self.exciting_patches = [[None for _ in range(self.num_filters)] for _ in range(self.num_conv_layers)]
-        self.patches_responses = [[None for _ in range(self.num_filters)] for _ in range(self.num_conv_layers)]
+        self.exciting_patches = [[None] * k for k in self.conv_layers]
+        self.patches_responses = [[None] * k for k in self.conv_layers]
+        self.flattened_exciting_patches = [[None] * k for k in self.conv_layers]
+        self.all_exciting_patches_at_layer = [None for _ in self.conv_layers]
 
         # Reshape input for convolution layers
         signal = tf.reshape(self.x, [self.mb_size, 28, 28, 1])
 
         # Apply convolution layers
-        for layer_no in range(self.num_conv_layers):
+        for layer_no in range(len(self.conv_layers)):
             # Initialize weights with st. dev = sqrt(2/N
             input_size = int(signal.get_shape()[1])
             w_init = tf.initializers.random_normal(stddev=sqrt(2 / input_size))
 
             # Build convolution layer
             cur_conv_layer = tf.layers.conv2d(inputs=signal,
-                                              filters=self.num_filters,
+                                              filters=self.conv_layers[layer_no],
                                               kernel_size=self.kernel_size,
                                               padding='same',
                                               activation=None,
@@ -74,8 +90,8 @@ class MnistTrainer:
             #   * Normalize.
             normalized_conv_layer = (cur_conv_layer - mb_mean)/mb_stdev
             #   * Scale and shift. Beta and Gamma are learned variables.
-            beta = tf.Variable(initial_value=tf.ones([1, 1, 1, self.num_filters], dtype=tf.float32))
-            gamma = tf.Variable(initial_value=tf.ones([1, 1, 1, self.num_filters], dtype=tf.float32))
+            beta = tf.Variable(initial_value=tf.ones([1, 1, 1, self.conv_layers[layer_no]], dtype=tf.float32))
+            gamma = tf.Variable(initial_value=tf.ones([1, 1, 1, self.conv_layers[layer_no]], dtype=tf.float32))
             cur_conv_layer = normalized_conv_layer * beta + gamma
 
             # Apply activation to convolved (and batch normalized) signal.
@@ -85,15 +101,16 @@ class MnistTrainer:
             cur_pool = tf.layers.max_pooling2d(inputs=cur_conv_layer, pool_size=[2, 2], strides=2)
 
             # Find exciting patches
-            for filter_no in range(self.num_filters):
+            for filter_no in range(self.conv_layers[layer_no]):
                 inp = 28 // (2**layer_no)  # Square root of input shape for current layer.
 
                 # Find top 10 responses to current filter, in the current mini-batch.
-                single_filtered_flattened = tf.reshape(cur_conv_layer[:, :, :, filter_no], [self.mb_size * inp * inp])
+                single_filtered_flattened = tf.reshape(cur_conv_layer[:, :, :, filter_no],
+                                                       [self.mb_size * inp * inp])
                 top10_vals, top10_indices = tf.nn.top_k(single_filtered_flattened,
                                                         k=10,
                                                         sorted=True)
-                top10_reshaped = tf.map_fn(lambda i: [i//(inp**2), (i//inp) % inp, i % inp],
+                top10_reshaped = tf.map_fn(lambda sxy: [sxy//(inp**2), (sxy//inp) % inp, sxy % inp],
                                            top10_indices,
                                            dtype=[tf.int32, tf.int32, tf.int32])
 
@@ -122,22 +139,32 @@ class MnistTrainer:
 
                 # Flatten and concatenate the 10 patches to 2 dimensions for visualization.
                 if layer_no == 0:
+                    print(self.exciting_patches[layer_no][filter_no].get_shape())
                     flattened_patches_shape = [1] +\
-                                              [self.kernel_size[0],
-                                               10 * self.kernel_size[1]] +\
+                                              [10 * self.kernel_size[0],
+                                               self.kernel_size[1]] +\
                                               [1]
                 else:
+                    print(self.exciting_patches[layer_no][filter_no].get_shape())
                     flattened_patches_shape = [1] +\
-                                              [self.num_filters * self.kernel_size[0],
+                                              [self.conv_layers[layer_no-1] * self.kernel_size[0],
                                                10 * self.kernel_size[1]] +\
                                               [1]
                 # Write patches to summary.
-                patch_name = "exciting_patches_{0}_{1}".format(layer_no, filter_no)
+                patch_name = "exciting_patches_filter{0}".format(filter_no)
                 flattened_exciting_patches = tf.reshape(self.exciting_patches[layer_no][filter_no],
-                                                                        flattened_patches_shape,
-                                                                        name=patch_name)
+                                                        flattened_patches_shape,
+                                                        name=patch_name)
+                self.flattened_exciting_patches[layer_no][filter_no] = flattened_exciting_patches
                 tf.summary.image(patch_name,
-                                 flattened_exciting_patches)
+                                 flattened_exciting_patches,
+                                 family='exciting_layer{0}'.format(layer_no))
+            self.all_exciting_patches_at_layer[layer_no] = tf.concat(self.flattened_exciting_patches[layer_no], axis=2)
+            # Write patches to summary.
+            all_patches_name = "exciting_patches_layer{0}".format(layer_no)
+            tf.summary.image(all_patches_name,
+                             self.all_exciting_patches_at_layer[layer_no],
+                             family='exciting_all_layers')
 
             # Set pooled image as new signal
             signal = cur_pool
@@ -145,31 +172,34 @@ class MnistTrainer:
             # Write 2 summaries for each filter:
             #  * kernel
             #  * input image with applied convolution
-            for i in range(self.num_filters):
+            for i in range(self.conv_layers[layer_no]):
                 if layer_no == 0:
                     tmp_str = 'conv2d/kernel:0'
                 else:
                     tmp_str = 'conv2d_%d/kernel:0' % layer_no
                 kernel = [v for v in tf.global_variables() if v.name == tmp_str][0]
+                print(kernel.get_shape())
                 kernel = kernel[:, :, :, i]
                 if layer_no == 0:
                     kernel = tf.reshape(kernel, [1] + self.kernel_size + [1])
                     applied = tf.reshape(cur_conv_layer[0, :, :, i], [1, inp, inp, 1])
                 else:
                     kernel = tf.reshape(kernel, [1] +\
-                                                [self.kernel_size[0], self.kernel_size[1] * self.num_filters] +\
-                                                [1])
+                                                [self.kernel_size[0],
+                                                 self.kernel_size[1] * self.conv_layers[layer_no-1]] + [1])
                     applied = tf.reshape(cur_conv_layer[0, :, :, i], [1, inp, inp, 1])
                 tf.summary.image('conv{0}_filter{1}_kernel'.format(layer_no, i),
                                  kernel,
-                                 max_outputs=3)
+                                 family='kernels_layer{0}'.format(layer_no),
+                                 max_outputs=1)
                 tf.summary.image('conv{0}_filter{1}_applied'.format(layer_no, i),
                                  applied,
-                                 max_outputs=3)
+                                 family='convolved_layer_{0}'.format(layer_no),
+                                 max_outputs=1)
 
         # Flatten the signal for the use in dense layers
-        if self.num_conv_layers > 0:
-            signal = tf.reshape(signal, [self.mb_size, self.num_filters * (28 // (2 ** self.num_conv_layers)) ** 2])
+        if len(self.conv_layers) > 0:
+            signal = tf.reshape(signal, [self.mb_size, self.conv_layers[-1] * (28 // (2 ** len(self.conv_layers))) ** 2])
         else:
             signal = tf.reshape([self.mb_size, 784])
 
@@ -240,8 +270,11 @@ class MnistTrainer:
         """
         x_test, y_test = self.mnist.test.images, self.mnist.test.labels
         N = self.mnist.test.num_examples
+
+        # I have replaced all -1 with self.mb_size to be sure about sizes of all layers.
         assert N % self.mb_size == 0,\
             "Sorry, mb_size must divide the number of images in test set"
+
         results = np.array([0., 0.])
         for batch_no in range(N // self.mb_size):
             beg = batch_no * self.mb_size
@@ -252,59 +285,11 @@ class MnistTrainer:
         results /= N
         self.logger.info("(Test(final):   Loss: {0[0]}, accuracy: {0[1]}".format(results))
 
-    def find_exciting_patches(self) -> np.ndarray:
-        """
-        :return: np array of shape (self.num_conv_layers, self.filters, 10), containing,
-            for each filter of each convolutional layer, 10 most exciting patches among
-            all training images.
-        TODO: store in a variable and write to summary
-        """
-        x_train, y_train = self.mnist.train.images, self.mnist.train.labels
-        N = self.mnist.train.num_examples
-        assert N % self.mb_size == 0, \
-            "Sorry, mb_size must divide the number of images in train set"
-        top10 = [[[] for _ in range(self.num_filters)] for _ in range(self.num_conv_layers)]
-        for batch_no in range(N // self.mb_size):
-            self.logger.debug(batch_no, "/", N // self.mb_size)
-            # Run model on single batch. Do not fetch self.train_op.
-            beg = batch_no * self.mb_size
-            end = min(N, (batch_no + 1) * self.mb_size)
-            batch_x, batch_y = x_train[beg:end], y_train[beg:end]
-            results = self.sess.run([self.patches_responses, self.exciting_patches],
-                                    feed_dict={self.x: batch_x, self.y: batch_y})
-            # For each layer and filter, update the current top 10 exciting patches,
-            # comparing them with patches found in last mini batch.
-            for layer_no in range(self.num_conv_layers):
-                for filter_no in range(self.num_filters):
-                    results[0][layer_no][filter_no] = results[0][layer_no][filter_no].reshape(10).tolist()
-                    tmp = list(zip(results[0][layer_no][filter_no], results[1][layer_no][filter_no]))
-                    top10[layer_no][filter_no] += tmp
-                    top10[layer_no][filter_no].sort(key=lambda a: a[0], reverse=True)
-                    top10[layer_no][filter_no] = top10[layer_no][filter_no][:10]
-
-        # Drop responses for exciting patches.
-        for layer_no in range(self.num_conv_layers):
-            for filter_no in range(self.num_filters):
-                for k in range(10):
-                    top10[layer_no][filter_no][k] = top10[layer_no][filter_no][k][1]
-
-        for layer_no in range(self.num_conv_layers):
-            for filter_no in range(self.num_filters):
-                self.logger.debug(top10[layer_no][filter_no])
-                tf.Variable(top10[layer_no][filter_no],
-                            validate_shape=False,
-                            name="top10_exciting_patches_layer{0}_filter{1}".format(layer_no, filter_no))
-
-        return top10
-
-    def train(self) -> None:
+    def train_and_evaluate(self) -> None:
         """
         Train and evaluate model.
         """
         with tf.Session() as self.sess:
-            # Set number of epochs.
-            nb_epochs = 1000
-
             # Initialize computation graph.
             self.create_model()
 
@@ -312,14 +297,14 @@ class MnistTrainer:
             tf.global_variables_initializer().run()
 
             # Initialize summary writer.
-            self.writer = tf.summary.FileWriter(logdir='summary')
+            self.writer = tf.summary.FileWriter(logdir='conv_vis')
 
-            for epoch_no in range(nb_epochs):
+            for epoch_no in range(self.nb_epochs):
                 # Train model on next batch
                 batch_x, batch_y = self.mnist.train.next_batch(self.mb_size)
                 results = self.train_on_batch(batch_x, batch_y, global_step=epoch_no)
 
-                if epoch_no % 10000 == 0:
+                if epoch_no > 0 and epoch_no % self.lr_decay_time == 0:
                     # Test on all samples.
                     self.test_on_all()
                     # Perform learning rate decay.
@@ -329,7 +314,6 @@ class MnistTrainer:
                     batch_x_t, batch_y_t = self.mnist.test.next_batch(self.mb_size)
                     test_results = self.test_on_batch(batch_x_t, batch_y_t)
                     self.logger.info("(Test(batch):   Loss: {0[0]}, accuracy: {0[1]}".format(test_results))
-            #self.find_exciting_patches()
             self.test_on_all()
 
             # Save the trained model with all valuable variables.
@@ -339,9 +323,9 @@ class MnistTrainer:
 
 if __name__ == '__main__':
     trainer = MnistTrainer(
-        num_conv_layers=2,
+        conv_layers=[10, 20],
         dense_layers=[128] + [10],
         kernel_size=[5, 5],
-        num_filters=10
+        nb_epochs=100000
     )
-    trainer.train()
+    trainer.train_and_evaluate()
